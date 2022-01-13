78635b68278822a5acc60800e5b11a9134b10e58
LaVi1002 <lavanya.nagarajan@zennzealinc.com>
2022-01-12 16:21:15 -0800

    task

---
+++ missing numbers.js
@@ -0,0 +1,14 @@
+let a = [1, 3, 5, 7, 9],
+  count = 10,
+  data = []
+
+for (let i = 1; i <= count; i++) {
+  if (a.indexOf(i) === -1) {
+    .push(i)
+  }
+}
+console.log(missing)
+
+
+OUTPUT:
+[ 2, 4, 6, 8, 10 ]
